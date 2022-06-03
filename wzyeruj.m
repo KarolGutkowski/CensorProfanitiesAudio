676f4b165b2c39f1132da0f150774a025a38934c
KarolGutkowski <90787864+KarolGutkowski@users.noreply.github.com>
2022-06-04 01:41:22 +0200

    Update wzyeruj.m

--- wzyeruj.m
+++ wzyeruj.m
@@ -1,9 +1,9 @@
-[y,fs] = audioread('k.wav');
+[y,fs] = audioread('audio_input_name.wav');
 Spill1 = importdata('output.txt');
 for i=1:2:length(Spill1)
     y(Spill1(i)*fs:Spill1(i+1)*fs,:) = 0;
 end
 
-audiowrite('10Clean.wav',y,fs);
+audiowrite('audio_output_name.wav',y,fs);
 
        
